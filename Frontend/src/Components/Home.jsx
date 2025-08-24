01013d727b859c6798841e3bbfe256a0334f0547
Sudhansu281 <159606194+Sudhansu281@users.noreply.github.com>
2025-08-24 12:27:44 +0530

    Update Home.jsx

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -6,7 +6,7 @@ import ChatArea from "./ChatArea";
 import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080");
+const socket = io("https://quick-chat-18.onrender.com");
 
 function Home() {
   const { selectedChat, user } = useSelector((state) => state.userReducer);
